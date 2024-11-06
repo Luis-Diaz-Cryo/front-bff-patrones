eda6d0ab95ac9dc82982a9a332f68cacca5f5d5b
cylde <cyldecryo@gmail.com>
2024-11-06 08:20:02 -0500

    prueba con nodeport

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -4,8 +4,9 @@ function App() {
   const [data, setData] = useState(null);
   //http://127.0.0.1:30011
   //http://localhost:5001/api/data
+  //http://192.168.49.2:30011
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/data')
+    fetch('http://192.168.49.2:30011/api/data')
       .then(response => response.json())
       .then(setData);
   }, []);
