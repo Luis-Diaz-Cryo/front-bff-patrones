9888f1ebbe0f265366b9c04ca9396341192bb46e
cylde <cyldecryo@gmail.com>
2024-11-06 07:10:06 -0500

    cambio de ip

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
-
+  //http://127.0.0.1:30011
   useEffect(() => {
     fetch('http://127.0.0.1:30011/api/data')
       .then(response => response.json())
