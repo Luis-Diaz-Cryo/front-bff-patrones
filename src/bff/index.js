cc2721f0f4676eb001a678c735091cf38575de40
cylde <cyldecryo@gmail.com>
2024-11-07 14:38:53 -0500

    implementacion de cors

--- src/bff/index.js
+++ src/bff/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+
+app.use(cors());
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from it is i kevin!' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`BFF running on port ${PORT}`));
-
-
+app.listen(5000, () => {
+  console.log('BFF service running on port 5000');
+});
